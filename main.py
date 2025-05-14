ab1125455d68915f0912bb16379f0a4960edf9fc
Roberta Bissoli <robertabissoli03775@gmail.com>
2025-05-14 15:50:56 -0300

    segunda etapa concluida

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ templates = Jinja2Templates(directory="templates")
 
 produto_repo.criar_tabela()
 cliente_repo.criar_tabela()
-
+categoria_repo.criar_tabela()
 
 @app.get("/")
 async def get_root():
@@ -33,6 +33,12 @@ async def get_clientes():
 async def get_produtos():
     produtos = produto_repo.obter_todos()
     response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
+    return response
+
+@app.get("/categorias")
+async def get_categorias():
+    categorias = categoria_repo.obter_todos()
+    response = templates.TemplateResponse("categoria.html", {"request": {}, "categorias": categorias})
     return response
 
 
